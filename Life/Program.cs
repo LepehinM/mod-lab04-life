60db1d563311663de3cff819e190c30090fea1bb
Makar <anl-pr@yandex.ru>
2022-05-02 21:52:43 +0300

    comment

--- Life/Program.cs
+++ Life/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
-using Newtonsoft.Json;
+using System.Text.Json;
 using System.IO;
 
 namespace cli_life
